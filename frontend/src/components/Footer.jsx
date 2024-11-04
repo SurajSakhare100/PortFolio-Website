c38f3a3766d9e57400ef2281be2c15e9fa526d0d
suraj sakhare <sakharesuraj10@gmail.com>
2024-11-04 11:05:13 +0530

    readspark backlink added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
     <div className='w-full  text-white'>
           <div className="hidden md:block absolute -bottom-[10%] right-[10%] w-96 h-44 scale-[300%] bg-[#e14affc2] rounded-full blur-3xl  opacity-50 z-0"></div>
           <div className="hidden md:block absolute -bottom-[10%] left-[15%] w-96  h-44 scale-[300%] bg-[#e14affc1] rounded-full blur-3xl  opacity-50 z-0"></div>
-          <div className='bg-black w-full h-20 absolute bottom-0 flex items-center justify-center'>
+          <div className='bg-black w-full h-20 absolute bottom-0 flex flex-col items-center justify-center'>
           © {new Date().getFullYear()} Suraj Sakhare. All rights reserved.
+          <p>Also Visit to ReadSpark : <a href="https://readspark.vercel.app/" target="_blank"><span className='font-bold'>ReadSpark</span></a></p>
           </div>
           <div>
-          <p>Also Visit to Dev Tool: <a href="https://toolfordev.vercel.app/" target="_blank">DevTool</a></p>
           </div>
     </div>
   )
